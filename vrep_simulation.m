61ff0fe5ab056fb949ae6bbb4528465a0a9a56b2
karadalex <karadalex@gmail.com>
2019-05-06 05:37:18 +0300

    load frames and solve IK for these

--- vrep_simulation.m
+++ vrep_simulation.m
@@ -1,16 +1,25 @@
-% IMPORTANT: for each successful call to simxStart, there
-% should be a corresponding call to simxFinish at the end!
+%% Load target positions, orientations and paths
+% !! TIME CONSUMING: 
+% run once and save in workspace
+referenceFrames
+generalSolutions = invKinSym(M_joints);
+% solve IK for start and target positions of bottles
+startSol = zeros(64,6,4);
+targetSol = zeros(64,6,4);
+for i = 1:1:4
+    startSol(:,:,i) = ikSolutionSet(M_0_6_bis(:,:,i), generalSolutions);
+    targetSol(:,:,i) = ikSolutionSet(M_0_6_bit(:,:,i), generalSolutions);
+end
+
 
 %% Start connection to V-REP
+% IMPORTANT: for each successful call to simxStart, there
+% should be a corresponding call to simxFinish at the end!
 disp('Program started');
 % vrep=remApi('remoteApi','extApi.h'); % using the header (requires a compiler)
 vrep=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
 vrep.simxFinish(-1); % just in case, close all opened connections
 clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
-
-
-%% Load target positions, orientations and paths
-referenceFrames
 
 
 %% Send commands to V-REP remoteApiServer
@@ -19,12 +28,7 @@ if (clientID>-1)
 
     % Command UR10 robot
     targetPosition = [90*pi/180, 90*pi/180, -90*pi/180, 90*pi/180, 90*pi/180, 90*pi/180];
-    [res, retInts, retFloats, retStrings, retBuffer]=vrep.simxCallScriptFunction(clientID,'UR10',vrep.sim_scripttype_childscript,'commandUR10',[],targetPosition,[],[],vrep.simx_opmode_blocking);
-    if (res==vrep.simx_return_ok)
-        fprintf('Code execution returned: %s\n',retStrings);
-    else
-        fprintf('Remote function call failed\n');
-    end
+    moveRobot(targetPosition)
 
 else
     disp('Failed connecting to remote API server');
